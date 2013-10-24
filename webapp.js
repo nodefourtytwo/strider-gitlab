5fc364afd463b7e32a00949b902394111942f959
Joël Dimbernat <j.dimbernat@gibmedia.fr>
2013-10-24 16:44:10 +0200

    Working list of repos and adding a project

--- webapp.js
+++ webapp.js
@@ -1,12 +1,19 @@
 
 var api = require('./lib/api');
+var parser = require('url');
 
 module.exports = {
 
   listRepos: function (userConfig, next) {
     api.get(userConfig,"projects",function(data){
-      console.log(typeof data);
-      next(null, data.map(api.parseRepo).filter(function(repo){
+      var repos = new Array();
+      var parsed = parser.parse(userConfig.api_url);
+      data.forEach(function(repo){
+        repo.org = parsed.hostname;
+        repo.display_url = parsed.protocol+'//'+parsed.hostname+'/';
+        repos.push(repo);
+      })
+      next(null, repos.map(api.parseRepo).filter(function(repo){
         return repo.config.scm === 'git';
       }))
     });

2ac7359a49a49dd5559823d4ca6dc9cc341070b8
Joel Dimbernat <j.dimbernat@gibmedia.fr>
2014-08-05 11:21:44 +0200

    Recursive calls allow to get rid of the 100 documents per page limit

--- webapp.js
+++ webapp.js
@@ -5,9 +5,9 @@ var parser = require('url');
 module.exports = {
 
   listRepos: function (userConfig, done) {
-    api.get(userConfig,"projects",function(err,data){
+    api.get_r(userConfig,"projects",function(err,data){
       if(!err){
-        repos = JSON.parse(data);
+        var repos = JSON.parse(data);
         done(null, repos.map(api.parseRepo).filter(function(repo){
           return repo.config.scm === 'git';
         }));
